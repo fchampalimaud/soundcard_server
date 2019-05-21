12e1d7c476c3b1dcd295c146e340c23909e26ae0
Luís Teixeira <micboucinha@gmail.com>
2019-05-21 15:43:22 +0100

    Added metadata to header of first command on client

--- client.py
+++ client.py
@@ -8,6 +8,11 @@ def tcp_send_sound_client(loop):
     reader, writer = yield from asyncio.open_connection('localhost', 9999,
                                                         loop=loop)
 
+    sound_index = 4
+    duration = 4
+    sample_rate = 96000
+    data_type = 0
+
     # generate sound
     window_config = WindowConfiguration( left_duration = 0,
                                      left_apply_window_start = True,
@@ -18,10 +23,10 @@ def tcp_send_sound_client(loop):
                                      right_apply_window_end = True,
                                      right_window_function = 'Bartlett')
 
-    wave_int = generate_sound(fs=96000,                 # sample rate in Hz
-                              duration=4,               # duration of the sound in seconds
-                              frequency_left=1500,      # frequency of the sinusoidal signal generated in Hz for the left channel
-                              frequency_right=1200,     # frequency of the sinusoidal signal generated in Hz for the right channel
+    wave_int = generate_sound(fs=sample_rate,                 # sample rate in Hz
+                              duration=duration,              # duration of the sound in seconds
+                              frequency_left=1500,            # frequency of the sinusoidal signal generated in Hz for the left channel
+                              frequency_right=1200,           # frequency of the sinusoidal signal generated in Hz for the right channel
                               window_configuration=window_config
                               )
 
@@ -44,7 +49,10 @@ def tcp_send_sound_client(loop):
     header[:preamble_size] = [2, 255, int('0x10', 16), int('0x88', 16), 128, 255, 1 ]
 
     # TODO: this should be extracted from here
-    # TODO: add metadata information to header
+    metadata = np.array([sound_index, duration, sample_rate, data_type ], dtype=np.int32)
+    header[preamble_size: preamble_size + metadata_size] = metadata.view(np.int8)
+
+    #TODO: filemetadata
 
 
     # add first block of data to header
@@ -72,15 +80,16 @@ def tcp_send_sound_client(loop):
         # write data from wave_int to cmd
         wave_idx = i * 32768
         data_block = wave_int8[wave_idx: wave_idx + 32768]
+
+        # clean the remaining elements for the last chunk which might be smaller than 32K
         if i == commands_to_send - 1:
             data_cmd[data_cmd_data_index:] = 0
         data_cmd[data_cmd_data_index: data_cmd_data_index + len(data_block)] = data_block
 
-        # sum all bytes as a byte and save that in the last index as the checksum
-        # clean last byte so we get the correct checksum
+        # clean last byte so we can calculate the correct checksum
         data_cmd[-1] = 0
+        # sum all bytes with overflow to calculate the checksum
         data_cmd[-1] = data_cmd.sum(dtype=np.int8)
-        #print(f'checksum idx: {i}, {data_cmd[-1].view(np.uint8)}')
 
         # write to socket
         writer.write(bytes(data_cmd))

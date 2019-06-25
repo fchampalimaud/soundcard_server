8093afe03044026bc2fa174d5ca2910d69c7336a
Luís Teixeira <micboucinha@gmail.com>
2019-06-25 17:28:26 +0100

    Added partial test for the metadata information

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,5 +1,7 @@
+import numpy as np
 from client import ClientSoundCard
 from generate_sound import generate_sound, WindowConfiguration
+
 
 import pytest
 
@@ -49,3 +51,24 @@ async def test_header_size_without_data_and_without_file_metadata(prepare_sound)
     client.prepare_header(with_data=False, with_file_metadata=False)
 
     assert len(client.header) == (5 + 16 + 1)
+
+#TODO: check metadata contents
+@pytest.mark.asyncio
+async def test_metadata_contents(prepare_sound):
+    sound_index = 4
+    duration = 12
+    sample_rate = 96000
+    data_type = 0
+    metadata_index = 7
+
+    client = ClientSoundCard(prepare_sound)
+    client.prepare_header(with_data=True, with_file_metadata=True)
+    client.add_metadata([sound_index, client.sound_file_size_in_samples, sample_rate, data_type])
+
+    sound_index_on_header = client.header[metadata_index: metadata_index + 4].view(np.int32)
+    assert sound_index == sound_index_on_header[0]
+
+    sound_file_size_in_samples_on_header = client.header[metadata_index + 4: metadata_index + 4 + 4].view(np.int32)
+    assert client.sound_file_size_in_samples == sound_file_size_in_samples_on_header[0]
+
+

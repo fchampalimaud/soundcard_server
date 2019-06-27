a96a1c1b0637635c243cb825822325520f681f18
Luís Teixeira <micboucinha@gmail.com>
2019-06-27 11:01:26 +0100

    Added initial tests for the filemetadata information

--- tests/test_client.py
+++ tests/test_client.py
@@ -74,3 +74,63 @@ async def test_metadata_contents(prepare_sound, sound_index, duration, sample_ra
     data_type_on_header = client.header[metadata_index + 4 * 3: metadata_index + 4 * 3 + 4].view(np.int32)
     assert data_type == data_type_on_header
 
+@pytest.mark.asyncio
+@pytest.mark.parametrize('sound_filename_str', 
+                         ['testing.filename.bin', 
+                          'with some unicode chars á é í ó ú @', 
+                          '.', 
+                          '',
+                          'extremely long string so that it might go over the size limit. let\'s repeat that so we might actually reach the maximum value. extremely long string so that it might go over the size limit.'])
+async def test_file_metadata_sound_filename(prepare_sound, sound_filename_str):
+    file_metadata_index = 0
+    max_dimension = 169
+
+    client = ClientSoundCard(prepare_sound(2, 4, 96000, 1))
+    client.prepare_header(with_data=True, with_file_metadata=True)
+
+    client.add_sound_filename(sound_filename_str)
+
+    sound_filename_on_header = client.filemetadata[file_metadata_index: file_metadata_index + max_dimension]
+    as_str = sound_filename_on_header.tobytes().strip(b'\0')
+    assert sound_filename_str[:max_dimension] == "".join(map(chr, as_str))
+ 
+@pytest.mark.asyncio
+@pytest.mark.parametrize('testing_metadata_name', 
+                         ['testing.metadata.bin', 
+                          'with some unicode chars á é í ó ú @', 
+                          '.', 
+                          '',
+                          'extremely long string so that it might go over the size limit. let\'s repeat that so we might actually reach the maximum value. extremely long string so that it might go over the size limit.'])
+async def test_file_metadata_with_metadata_filename(prepare_sound, testing_metadata_name):
+    file_metadata_index = 170
+    max_dimension = 169
+
+    client = ClientSoundCard(prepare_sound(2, 4, 96000, 1))
+    client.prepare_header(with_data=True, with_file_metadata=True)
+
+    client.add_metadata_filename(testing_metadata_name)
+
+    metadata_filename_on_header = client.filemetadata[file_metadata_index: file_metadata_index + max_dimension]
+    as_str = metadata_filename_on_header.tobytes().strip(b'\0')
+    assert testing_metadata_name[:max_dimension] == "".join(map(chr, as_str))
+ 
+@pytest.mark.asyncio
+@pytest.mark.parametrize('testing_description_name', 
+                         ['testing.description.bin', 
+                          'with some unicode chars á é í ó ú @', 
+                          '.', 
+                          '',
+                          'extremely long string so that it might go over the size limit. let\'s repeat that so we might actually reach the maximum value. extremely long string so that it might go over the size limit.'])
+async def test_file_metadata_with_description_filename(prepare_sound, testing_description_name):
+    description_filename_index = 340
+    max_dimension = 169
+
+    client = ClientSoundCard(prepare_sound(2, 4, 96000, 1))
+    client.prepare_header(with_data=True, with_file_metadata=True)
+
+    client.add_description_filename(testing_description_name)
+
+    metadata_filename_on_header = client.filemetadata[description_filename_index: description_filename_index + max_dimension]
+    as_str = metadata_filename_on_header.tobytes().strip(b'\0')
+    assert testing_description_name[:max_dimension] == "".join(map(chr, as_str))
+ 

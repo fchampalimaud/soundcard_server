fd941f6244f16a406f07b476be760dbdf0ab0378
Luís Teixeira <micboucinha@gmail.com>
2019-08-12 16:46:18 +0100

    Mean time for each chunk sent is presented in ms

--- server.py
+++ server.py
@@ -326,7 +326,7 @@ class SoundCardTCPServer(object):
             pbar.update()
 
         pbar.close()
-        print(f'\tMean time for sending each chunk: {np.mean(chunk_sending_timings)} s')
+        print(f' Mean time for sending each chunk: {int(round(np.mean(chunk_sending_timings)*1000))} ms')
 
         writer.write('OK'.encode())
         print(f'File successfully sent in {time.time() - initial_time} s{os.linesep}')
